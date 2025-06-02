263af63a0cf58e2bd4b47558697670a254410347
Zubair Gulbarge <71716740+zubair-gulbarge@users.noreply.github.com>
2025-06-02 22:43:14 +0000

    added datatypes notes.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";       // treat all js code a newer version.
+
+// alert(3+3)      // you cannot use it like this here, it'll work only in browser[we are using nodejs].
+
+// code should be readable and futureproof 
+// console.log(3 +3) console.log('Luffy')          //This'll throw an error.
+console.log(3 +3); console.log('Luffy')         // This won't give you en error.
+// code readability should be high.
+
+let name = "Zoro"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => mostly used in react or figma tools (for defining uniqueness)
+
+// object
+
+console.log(typeof 'zoro');
+console.log(typeof null);       // it'll show as object.
+console.log(typeof undefined);  // it'll show as undefined.
