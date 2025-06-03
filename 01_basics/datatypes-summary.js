7b270c99bdfe8a440554352ab07969afe9bddcf7
Zubair Gulbarge <71716740+zubair-gulbarge@users.noreply.github.com>
2025-06-03 21:24:35 +0000

    updated

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// primitive & non-primitive
+
+// call by value or call by Reference
+
+// How it's stored in memory 
+
+// Primitive - 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference Type (Non primitive) : Array, Objects, Functions
+
+// javascript objects, and browser web events or browser event to master javascript.
+
+//  Is javascript dynamicallay typed language or statically typed language
+
+const score = 100           // normal javascript
+// const age:number = 55       // this is typescript method or syntax
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userEmail;                          // undefined
+
+// ---
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 120943718927461641829437128394714931873409164n
+
+
+const heroes = ["IronMan", "Batman"]
