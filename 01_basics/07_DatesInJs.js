d87c464e76d6221d6ec850236585ffe33c1120c8
Zubair Gulbarge <71716740+zubair-gulbarge@users.noreply.github.com>
2025-06-10 17:32:20 +0000

    Date function

---
+++ 01_basics/07_DatesInJs.js
@@ -0,0 +1,24 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);     // Object
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate)
+// console.log(myCreatedDate.toDateString())
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// console.log(myCreatedDate.toLocaleString())
+
+// let myCreatedDate = new Date("2024-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
