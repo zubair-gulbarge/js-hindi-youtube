ae521ba1e8664b8cca9a5c332a4ba40292e3f2b2
Zubair Gulbarge <71716740+zubair-gulbarge@users.noreply.github.com>
2025-06-10 17:48:42 +0000

    Updated the Date Notes

--- 01_basics/07_DatesInJs.js
+++ 01_basics/07_DatesInJs.js
@@ -20,5 +20,22 @@
 let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString())
 
-let myTimeStamp = Date.now()
-console.log(myTimeStamp)
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());        // 5 - in date dunction month start from 0 [0 = jan, 1 = feb]
+console.log(newDate.getDay());
+
+
+// Important Very Usefull.
+
+newDate.toLocaleString('default', {
+    weekday: "long"                         // CTRL + Space for suggesion, this is to modify date further.
+})
